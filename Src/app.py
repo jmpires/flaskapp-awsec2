448333771abf910240358978c0d01fcbc77c879f
Jorge Pires <jorgepires.email@gmail.com>
2024-09-20 16:56:24 +0100

    playing changing the .py code

--- Src/app.py
+++ Src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1> Hello World!</h1><br><p>Flask App is Up & Running!</p> '
+    return '<h1> Hello World!</h1><br><h1>Flask App is Up & Running!</h1> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
