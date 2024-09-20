9ca76b207a4144317e121478beff4d8ac5b1641a
Jorge Pires <jorgepires.email@gmail.com>
2024-09-20 16:01:17 +0100

    appT05

--- Src/app.py
+++ Src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1> This is Akhilesh Mishra</h1><br><p>Thank you for reading. I hope you enjoyed it, follow for more content around Devops.</p> '
+    return '<h1> Hello World!</h1><br><p>Flask app is Up 6 Running!</p> '
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
